af18dde17a4313ec4e4fde8f1cda2ae86342182f
dark1922 <jvdematos004@gmail.com>
2021-07-01 14:08:20 -0300

    feath: iniciando o projeto

---
+++ enviando-mail/src/test/java/enviando/email/AppTest.java
@@ -0,0 +1,12 @@
+package enviando.email;
+
+public class AppTest {
+
+	@org.junit.Test //mesmo que o método main
+  public void testeEmail() {
+	  
+		//Olhe as configurações smtp do seu email
+	  
+  } 
+  
+}
