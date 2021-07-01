00437960624aba3edf03ccfcdbdd08a787eec49f
dark1922 <jvdematos004@gmail.com>
2021-07-01 16:23:05 -0300

    feath: propriedades de conexão

--- enviando-mail/src/test/java/enviando/email/AppTest.java
+++ enviando-mail/src/test/java/enviando/email/AppTest.java
@@ -1,5 +1,7 @@
 package enviando.email;
 
+import java.util.Properties;
+
 public class AppTest {
 
 	@org.junit.Test //mesmo que o método main
@@ -7,6 +9,13 @@ public class AppTest {
 	  
 		//Olhe as configurações smtp do seu email
 	  
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true"); //autorização
+		properties.put("mail.smtp.starttls", "true");//autenticação
+		properties.put("mail.smtp.host", "smtp.gmail.com");//servidor do gmail do Google
+		properties.put("mail.smtp.port", "465"); //porta do servidor
+		properties.put("mail.smtp.socketFactory.port", "465");//Expecifica a porta a ser conectada pelo socket
+		properties.put("mail.smtp.socketFactoryClass", "javax.net.ssl.SSLSocketFactory");//classe socket de conexão ao SMTP
   } 
   
 }
