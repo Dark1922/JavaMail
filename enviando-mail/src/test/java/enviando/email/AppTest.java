1ccbea378d72499e9ea8e824d93423baf31acf2d
dark1922 <jvdematos004@gmail.com>
2021-07-01 23:53:05 -0300

    feath: base de enviar email

--- enviando-mail/src/test/java/enviando/email/AppTest.java
+++ enviando-mail/src/test/java/enviando/email/AppTest.java
@@ -2,14 +2,19 @@ package enviando.email;
 
 import java.util.Properties;
 
+import javax.mail.Address;
 import javax.mail.Authenticator;
+import javax.mail.Message;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class AppTest {
 
-	private String userName = "seuemail@gmail.com";
-	private String senha = "suaSenhaGmail";
+	private String userName = "jvdematos005@gmail.com";
+	private String senha = "password";
 	
 	@org.junit.Test // mesmo que o método main
 	public void testeEmail() {
@@ -22,7 +27,7 @@ public class AppTest {
 			properties.put("mail.smtp.host", "smtp.gmail.com");// servidor do gmail do Google
 			properties.put("mail.smtp.port", "465"); // porta do servidor
 			properties.put("mail.smtp.socketFactory.port", "465");// Expecifica a porta a ser conectada pelo socket
-			properties.put("mail.smtp.socketFactoryClass", "javax.net.ssl.SSLSocketFactory");// classe socket de conexão// ao SMTP
+			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");// classe socket de conexão// ao SMTP
 				
 			//session do javamail depedencia colocada no pom.xml
             Session session = Session.getInstance(properties, new Authenticator() {
@@ -32,7 +37,16 @@ public class AppTest {
             		return new PasswordAuthentication(userName, senha);
             	}
 			});
+            //array de adrees pode mandar pra varios email users , pra quem vai ser enviado aqui
+            Address[] toUser = InternetAddress.parse("jvdematos004@gmail.com"); 
 			
+            Message message = new MimeMessage(session); //session do usuario que vai enviar os email
+            message.setFrom(new InternetAddress(userName)); //quem está enviando o email
+            message.setRecipients(Message.RecipientType.TO, toUser); //email de destino os array de email
+            message.setSubject("Ceghou o email Enviado com Java"); //assunto do email titulo qnd envia
+            message.setText("Ola programador, você acaba de receber um e-mail enviado com Java do curso Formação Java Web do Alex");//texto do envio do email
+          
+            Transport.send(message); //objeto de mensagem pra ser enviado
 			
 		} catch (Exception e) {
 			e.printStackTrace();
